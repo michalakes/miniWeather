5812deae2e679e1c65b4f1b6974a0fb98ea2aba9
john@michalakes.us <michalak@ucar.edu>
2022-07-18 15:24:53 -0600

    On branch jm Added verification code and tested. Gives:
      savoy:/home/michalak/miniWeather/cpp/build 1238 > !./jm
      ./jmparallelfor
      Quadro GV100
      ndof 145
      mrows 70
      lcblk 32
      ncblk 100
      memcheck  265459200
      ndof 145
      mrows 70
      kl 23
      ku 23
      compare afac n=324800
             Ndifs    RMS (1)            RMS (2)     DIGITS    RMSE pntwise max
          284071                 NaN   0.6159594550E+04 ***          NaN 0.0000E+00
    
    The test_vdgbsv_firstouch.F90 on Savoy (with fixes) gives:
    
      upload time in microsec:             1
      max threads            1
      rank                   0
      kernel time in microsec:       4398599
      dnload time in microsec:             0
      compare afac n=      324800
          99124    0.6159594550E+04   0.6159594550E+04  15   0.2149E-11 0.5260E-15
      compare bblk
           4254    0.4057818643E-01   0.4057818643E-01  15   0.2579E-15 0.1724E-13
    
    Since the second columns match, it's reading in the fort.99 verification
    set correctly.  Also checked to make sure that the results in the first
    column are the same if the call to vdgbsv is commented out in both
    codes.
    
    So this is kindof progress.
    
     Changes to be committed:
    	modified:   ../jm_mpi_parallelfor.cpp
    	modified:   ../reader.F90

--- cpp/jm_mpi_parallelfor.cpp
+++ cpp/jm_mpi_parallelfor.cpp
@@ -26,6 +26,9 @@ typedef yakl::Array<double,2,yakl::memDevice,yakl::styleFortran> doub2d;
 typedef yakl::Array<double,3,yakl::memDevice,yakl::styleFortran> doub3d;
 typedef yakl::Array<double,4,yakl::memDevice,yakl::styleFortran> doub4d;
 
+typedef yakl::Array<int    const,1,yakl::memDevice,yakl::styleFortran> intConst1d;
+typedef yakl::Array<int    const,2,yakl::memDevice,yakl::styleFortran> intConst2d;
+typedef yakl::Array<int    const,3,yakl::memDevice,yakl::styleFortran> intConst3d;
 typedef yakl::Array<real   const,1,yakl::memDevice,yakl::styleFortran> realConst1d;
 typedef yakl::Array<real   const,2,yakl::memDevice,yakl::styleFortran> realConst2d;
 typedef yakl::Array<real   const,3,yakl::memDevice,yakl::styleFortran> realConst3d;
@@ -56,9 +59,9 @@ struct Fixed_data {
   int nranks, myrank;         //Number of MPI ranks and my rank id
   int left_rank, right_rank;  //MPI Rank IDs that exist to my left and right in the global domain
   int mainproc;               //Am I the main process (rank == 0)?
-  realConst2d precond_build;       //   Dimensions: (lcblk, ncblk)
-  realConst2d ju;                  //   Dimensions: (lcblk, ncblk)
-  realConst2d jp;                  //   Dimensions: (lcblk, ncblk)
+  int2d precond_build;        //   Dimensions: (lcblk, ncblk)
+  int2d ju;                   //   Dimensions: (lcblk, ncblk)
+  int2d jp;                   //   Dimensions: (lcblk, ncblk)
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -87,6 +90,7 @@ void reductions           ( realConst3d state , double &mass , double &te , Fixe
 
 //Declaring the functions defined after "main"
 void init                 ( int3d &ipiv, real4d &afac, real3d &bblk, real &dt , Fixed_data &fixed_data );
+void verify               ( int3d &ipiv, real4d &afac, real3d &bblk, real &dt , Fixed_data &fixed_data );
 YAKL_INLINE void vdgbtf2( int ib, int n, int kl, int ku,
                           real4d const &ab,
                           int ldab,
@@ -117,6 +121,7 @@ int main(int argc, char **argv) {
     int2d  ju = int2d("ju",fixed_data.ncblk,LCBLK) ;
     int2d  jp = int2d("jp",fixed_data.ncblk,LCBLK) ;
 
+#if 1
     parallel_outer( "vdgbtf2", 
                              fixed_data.ncblk, 
                              YAKL_LAMBDA ( int ib, yakl::InnerHandler inner_handler )
@@ -131,6 +136,9 @@ int main(int argc, char **argv) {
                fixed_data,
                inner_handler ) ;
     }, yakl::LaunchConfig<LCBLK>() ) ;
+#endif
+
+    verify( ipiv, afac, bblk, dt, fixed_data );
 
   }
   yakl::finalize();
@@ -213,7 +221,6 @@ YAKL_INLINE void vdgbtf2( int ib,
 //           do ie = es, ee
 //             JU(M0DEX(ie)) = MAX( JU(M0DEX(ie)), MIN( J+KU+JP(M0DEX(ie))-1, N ) )
 //           enddo
-    int anyjp = 0 ;
     parallel_inner( yakl::fortran::Bounds<1>(LCBLK),[&] (int ie) {
       ipiv(ie,j,ib) = jp(ie,ib)+j-1 ;
       int j1 = ((j+ku+jp(ie,ib)-1)<n)?(j+ku+jp(ie,ib)-1):n ;
@@ -289,9 +296,10 @@ YAKL_INLINE void vdgbtf2( int ib,
   } // j loop
 }
 
-
 extern "C"{
 void read_scalars_(int*, int*, int*, int*, int*) ;
+void read_arrays_(int*, int*, int*, int*, int*, int*, int*, real*, real* ) ;
+void diff_(real*, real*, int* ) ;
 }
 
 void init( int3d &ipiv, real4d &afac, real3d &bblk, real &dt , Fixed_data &fixed_data ) {
@@ -335,8 +343,14 @@ void init( int3d &ipiv, real4d &afac, real3d &bblk, real &dt , Fixed_data &fixed
   ipiv          =  int3d( "ipiv" , lcblk, ndof, ncblk);
   afac          = real4d( "afac" , lcblk, mrows, ndof, ncblk) ;
   bblk          = real3d( "bblk" , lcblk, ndof, ncblk);
-  fixed_data.ju = real2d( "ju"   , lcblk, ncblk) ;
-  fixed_data.jp = real2d( "jp"   , lcblk, ncblk) ;
+  fixed_data.ju =  int2d( "ju"   , lcblk, ncblk) ;
+  fixed_data.jp =  int2d( "jp"   , lcblk, ncblk) ;
+  fixed_data.precond_build =  int2d( "precond_build"   , lcblk, ncblk) ;
+
+  int2dHost  precHost =  int2dHost( "precHost" , lcblk, ncblk);
+  int3dHost  ipivHost =  int3dHost( "ipivHost" , lcblk, ndof, ncblk);
+  real4dHost afacHost = real4dHost( "afacHost" , lcblk, mrows, ndof, ncblk) ;
+  real3dHost bblkHost = real3dHost( "bblkHost" , lcblk, ndof, ncblk);
 
   int iunit = 98 ;
   read_scalars_( &iunit,
@@ -350,5 +364,71 @@ void init( int3d &ipiv, real4d &afac, real3d &bblk, real &dt , Fixed_data &fixed
   fprintf(stderr,"kl %d \n",fixed_data.kl) ;
   fprintf(stderr,"ku %d \n",fixed_data.ku) ;
 
+  int *p_precond_build = precHost.data(); 
+  int *p_ipiv = ipivHost.data() ;
+  real *p_afac = afacHost.data() ;
+  real *p_bblk = bblkHost.data() ;
+
+  read_arrays_( &iunit, &lcblk, &ncblk
+               ,&fixed_data.ndof
+               ,&fixed_data.mrows
+               ,p_precond_build
+               ,p_ipiv
+               ,p_afac
+               ,p_bblk ) ;
+                
+  precHost.deep_copy_to(fixed_data.precond_build) ;
+  ipivHost.deep_copy_to(ipiv) ;
+  afacHost.deep_copy_to(afac) ;
+  bblkHost.deep_copy_to(bblk) ;
+  
+
 }
 
+void verify( int3d &ipiv, real4d &afac, real3d &bblk, real &dt , Fixed_data &fixed_data ) {
+  auto &nranks           = fixed_data.nranks          ;
+  auto &ndof             = fixed_data.ndof            ;
+  auto &mrows            = fixed_data.mrows           ;
+  auto &lcblk            = fixed_data.lcblk           ;
+  auto &ncblk            = fixed_data.ncblk           ;
+  auto &myrank           = fixed_data.myrank          ;
+  auto &mainproc         = fixed_data.mainproc        ;
+  auto &i_beg            = fixed_data.i_beg           ;
+  int  ierr;
+  int  memcheck ;
+
+// computed
+  int2dHost  precHost  =  int2dHost( "precHost1" , lcblk, ncblk);
+  int3dHost  ipivHost  =  int3dHost( "ipivHost1" , lcblk, ndof, ncblk);
+  real4dHost afacHost  = real4dHost( "afacHost1" , lcblk, mrows, ndof, ncblk) ;
+  real3dHost bblkHost  = real3dHost( "bblkHost1" , lcblk, ndof, ncblk);
+
+  afac.deep_copy_to(afacHost) ;
+  bblk.deep_copy_to(bblkHost) ;
+
+// read in
+  int2dHost  precHost2 =  int2dHost( "precHost2" , lcblk, ncblk);
+  int3dHost  ipivHost2 =  int3dHost( "ipivHost2" , lcblk, ndof, ncblk);
+  real4dHost afacHost2 = real4dHost( "afacHost2" , lcblk, mrows, ndof, ncblk) ;
+  real3dHost bblkHost2 = real3dHost( "bblkHost2" , lcblk, ndof, ncblk);
+
+  int iunit = 99 ;
+  int *p_precond_build = precHost2.data(); 
+  int *p_ipiv = ipivHost2.data() ;
+  real *p_afac = afacHost2.data() ;
+  real *p_bblk = bblkHost2.data() ;
+
+  read_arrays_( &iunit, &lcblk, &ncblk
+               ,&fixed_data.ndof
+               ,&fixed_data.mrows
+               ,p_precond_build
+               ,p_ipiv
+               ,p_afac
+               ,p_bblk ) ;
+
+  int n = lcblk*mrows*ndof ;
+printf("compare afac n=%d \n",n) ;
+
+  diff_( afacHost.data(), afacHost2.data(), &n ) ;
+
+}
--- cpp/reader.F90
+++ cpp/reader.F90
@@ -27,6 +27,7 @@ subroutine read_scalars(  &
   read(iunit) klower
   read(iunit) kupper
 end subroutine read_scalars
+
 subroutine read_arrays( &
     iunit       &
    ,lcblk       &
@@ -42,10 +43,11 @@ subroutine read_arrays( &
   integer, intent(in)  :: iunit,lcblk,ncblk,ndof,mrows
   integer, intent(out) :: prec_build(lcblk,ncblk)
   integer, intent(out) ::       ipiv(lcblk,ndof,ncblk)
-  real,    intent(out) ::       afac(lcblk,mrows,ndof,ncblk)
-  real,    intent(out) ::       bblk(lcblk,ndof,ncblk)
+  RTYPE,   intent(out) ::       afac(lcblk,mrows,ndof,ncblk)
+  RTYPE,   intent(out) ::       bblk(lcblk,ndof,ncblk)
   ! local
   integer              :: idummy, ib
+integer i,j,k
   rewind(iunit)
   read(iunit) idummy  ! ib
   read(iunit) idummy  ! es
@@ -54,15 +56,89 @@ subroutine read_arrays( &
   read(iunit) idummy  ! mrows
   read(iunit) idummy  ! klower
   read(iunit) idummy  ! kupper
-  read(iunit) prec_build(:,1)
+  if ( iunit .ne. 99 ) read(iunit) prec_build(:,1)
   read(iunit) ipiv(:,:,1)
   read(iunit) afac(:,:,:,1)
   read(iunit) bblk(:,:,1)
+
   do ib = 2, ncblk
-    prec_build(:,ib) = prec_build(:,1)
+    if ( iunit .ne. 99 ) prec_build(:,ib) = prec_build(:,1)
     ipiv(:,:,ib)     = ipiv(:,:,1)
     afac(:,:,:,ib)   = afac(:,:,:,1)
     bblk(:,:,ib)     = bblk(:,:,1)
   enddo
 end subroutine read_arrays
 
+
+subroutine diff ( a_in , b_in, n )
+
+  integer :: ifdiffs, n, dgts, ii
+  RTYPE   :: a_in(*), b_in(*)
+  RTYPE   :: a, b
+  double precision :: sumE, sum1, sum2, diff1, diff2, serr, perr, rmse, rms1, rms2, tmp1, tmp2
+
+
+  IFDIFFS=0
+  sumE = 0.0
+  sum1 = 0.0
+  sum2 = 0.0
+  diff1 = 0.0
+  diff2 = 0.0
+
+  do ii = 1, n 
+     a = a_in(ii)
+     b = b_in(ii)
+write(298,*)ii,a
+write(299,*)ii,b
+
+     ! borrowed from  Thomas Oppe's comp program
+     sumE = sumE + ( a - b ) * ( a - b )
+     sum1 = sum1 + a * a
+     sum2 = sum2 + b * b
+     diff1 = max ( diff1 , abs ( a - b ) )
+     diff2 = max ( diff2 , abs ( b ) )
+     IF (a .ne. b) then
+       IFDIFFS = IFDIFFS + 1
+     ENDIF
+     goto 60
+20   exit
+30   exit
+60   continue
+  enddo
+
+  rmsE = sqrt ( sumE / dble( n ) )
+  rms1 = sqrt ( sum1 / dble( n ) )
+  rms2 = sqrt ( sum2 / dble( n ) )
+  serr = 0.0
+  IF ( sum2 .GT. 0.0d0 ) THEN
+    serr = sqrt ( sumE / sum2 )
+  ELSE
+    IF ( sumE .GT. 0.0d0 ) serr = 1.0
+  ENDIF
+  perr = 0.0
+  IF ( diff2 .GT. 0.0d0 ) THEN
+    perr = diff1/diff2
+  ELSE
+    IF ( diff1 .GT. 0.0d0 ) perr = 1.0
+  ENDIF
+
+  IF ( rms1 - rms2 .EQ. 0.0d0 ) THEN
+    dgts = 15
+  ELSE
+    IF ( rms2 .NE. 0 ) THEN
+      tmp1 = 1.0d0/( ( abs( rms1 - rms2 ) ) / rms2 )
+      IF ( tmp1 .NE. 0 ) THEN
+        dgts = log10(tmp1)
+      ENDIF
+    ENDIF
+  ENDIF
+
+  IF (IFDIFFS .NE. 0 ) THEN
+    print 76
+    PRINT 77, IFDIFFS, rms1, rms2, dgts, rmsE, perr
+ 76 FORMAT (5x,2x,'Ndifs',4x,'RMS (1)',12x,'RMS (2)',5x,'DIGITS',4x,'RMSE',5x,'pntwise max')
+ 77 FORMAT ( 1x,I9,2x,e18.10,1x,e18.10,1x,i3,1x,e12.4,1x,e12.4 )
+  ENDIF
+  return
+  
+end subroutine diff
